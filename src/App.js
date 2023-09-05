4a415c94aec52ede36b721b0cc90d628054dc5fb
palmerigarrett <palmerigarrett@gmail.com>
2023-09-05 17:42:08 -0400

    remove node_modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import useCSV from './request/useCSV';
 
 function App (){
+  const initialFilterState = {
+    homeOwnership: 'All',
+    quarter: 'All',
+    term:  'All',
+    year: 'All'
+  };
+
+  const [filters, setFilters] = useState(initialFilterState);
   const aggregate = (data) => {
     const aggregateData = data.reduce((totals, item) => {
-      const { grade, currentBalance} = item;
+      const { grade, currentBalance, homeOwnership, quarter, term, year} = item;
       if (parseInt(grade)) {
         totals[grade] = (totals[grade] || 0) + parseFloat(currentBalance);
       }
@@ -16,10 +24,23 @@ function App (){
   const data = aggregate(useCSV());
   console.log({data});
 
+
+  // aggregate(data);
   return (
     <div className='App'>
       <p>HELLO WORLD</p>
       <p>CHARTS AND GRAPHS</p>
+      <table style={{border: "solid 1px black", borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            {Object.keys(data).map((grade) => {
+              console.log({grade});
+              return <th key={grade}>Grade {grade}</th>
+            })}
+          </tr>
+        </thead>
+
+      </table>
     </div>
   )
 }
