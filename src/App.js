765ee83ea70ba12a9f023edf82cff1ea3bb40085
palmerigarrett <palmerigarrett@gmail.com>
2023-09-06 12:03:34 -0400

    memoize aggregate function, use state to handle dropdowns, 2 decimals for amounts, dropsdowns now reflect in total amounts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import './App.css'
 import useCSV from './request/useCSV';
 
 function App (){
+  const data = useCSV();
   const initialFilterState = {
     homeOwnership: 'All',
     quarter: 'All',
     term:  'All',
     year: 'All'
   };
-
   const [filters, setFilters] = useState(initialFilterState);
-  
-  const aggregate = (data) => {
 
+  const [dropdownsState, setDropdownsState] = useState({
+    homeOwnership: new Set(),
+    quarter: new Set(),
+    term: new Set(),
+    year: new Set(),
+  });
+
+  
+  const aggregate = (data, getNewDropdowns) => {
     const dropdowns = {
       homeOwnership: new Set(),
       quarter: new Set(),
@@ -21,56 +28,80 @@ function App (){
       year: new Set(),
     };
 
-    const aggregateData = data.reduce((totals, item) => {
-      const { grade, currentBalance, homeOwnership, quarter, term, year} = item;
-      
-      // Add to dropdowns with O(1) complexity
-      homeOwnership && dropdowns.homeOwnership.add(homeOwnership);
-      quarter && dropdowns.quarter.add(quarter);
-      term && dropdowns.term.add(term);
-      year && dropdowns.year.add(year);
+    const aggregateData = useMemo(() => {
+      const agData = data.reduce((totals, item) => {
+        const { grade, currentBalance, homeOwnership, quarter, term, year} = item;
+        
+        // Add to dropdowns with O(1) complexity
+        homeOwnership && dropdowns.homeOwnership.add(homeOwnership);
+        quarter && dropdowns.quarter.add(quarter);
+        term && dropdowns.term.add(term);
+        year && dropdowns.year.add(year);
 
-      const homeOwnershipFilter = filters.homeOwnership === 'All' || homeOwnership === filters.homeOwnership;
-      const quarterFilter = filters.quarter === 'All' || quarter === filters.quarter;
-      const termFilter = filters.term === 'All' || term === filters.term;
-      const yearFilter = filters.year === 'All' || year === filters.year;
 
-      if (parseInt(grade)) {
-        if (homeOwnershipFilter && quarterFilter && termFilter && yearFilter) {
-          totals[grade] = (totals[grade] || 0) + parseFloat(currentBalance);
+        const homeOwnershipFilter = filters.homeOwnership === 'All' || homeOwnership === filters.homeOwnership;
+        const quarterFilter = filters.quarter === 'All' || quarter === filters.quarter;
+        const termFilter = filters.term === 'All' || term === filters.term;
+        const yearFilter = filters.year === 'All' || year === filters.year;
+
+        if (parseInt(grade)) {
+          if (homeOwnershipFilter && quarterFilter && termFilter && yearFilter) {
+            totals[grade] = (totals[grade] || 0) + parseFloat(currentBalance);
+          }
         }
-      }
-      return totals;
-    }, {});
-    return {data: aggregateData, dropdowns};
+        return totals;
+      }, {});
+      setDropdownsState(dropdowns);
+      return agData;
+    }, [data, filters]);
+    return aggregateData;
   };
-  const {data, dropdowns} = aggregate(useCSV());
+
+  const handleFilterChange = (event) => {
+    const {name, value} = event.target;
+    setFilters({...filters, [name]: value});
+  };
 
   return (
     <div className='App'>
       <p>HELLO WORLD</p>
       <p>CHARTS AND GRAPHS</p>
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(data).map((grade) => {
-              return <th key={grade}>Grade {grade}</th>
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {Object.entries(data).map(([grade, amount]) => {
-              const key = `${grade}-${amount}`
-              return <th key={key}>{amount}</th>
+      <section>
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(aggregate(data)).map((grade) => {
+                return <th key={grade}>Grade {grade}</th>
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {Object.entries(aggregate(data)).map(([grade, amount]) => {
+                const key = `${grade}-${amount}`
+                return <th key={key}>{amount.toFixed(2)}</th>
 
-            })}
-          </tr>
-        </tbody>
+              })}
+            </tr>
+          </tbody>
 
-      </table>
+        </table>
+      </section>
+      <section>
+        {Object.entries(dropdownsState).map(([name, dropdown]) => {
+          // console.log({name, dropdown})
+          return (
+            <select key={dropdown} name={name} onChange={handleFilterChange}>
+              <option name={name} value='All'>All</option>
+              {[...dropdown].map((item) => {
+                return <option key={item} value={item}>{item}</option>
+              })}
+            </select>
+          );
+        })}
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
